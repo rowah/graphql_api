785aa44eaaa605ef85843a45244e8d476774d5e7
Rowa <jrowah@gmail.com>
2024-10-14 16:11:56 +0300

    ensure email uniqueness and remove default primary key

--- priv/repo/migrations/20241014121824_create_users.exs
+++ priv/repo/migrations/20241014121824_create_users.exs
@@ -2,7 +2,8 @@ defmodule MediumGraphqlApi.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-    create table(:users) do
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
       add :first_name, :string
       add :last_name, :string
       add :email, :string
@@ -11,5 +12,7 @@ defmodule MediumGraphqlApi.Repo.Migrations.CreateUsers do
 
       timestamps(type: :utc_datetime)
     end
+
+    create unique_index(:users, [:email])
   end
 end
