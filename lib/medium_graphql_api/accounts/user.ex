4aa4ce25989c7a4e8038b5431595a4192675810c
Rowa <jrowah@gmail.com>
2024-10-14 16:13:00 +0300

    improve user input validation

--- lib/medium_graphql_api/accounts/user.ex
+++ lib/medium_graphql_api/accounts/user.ex
@@ -2,12 +2,16 @@ defmodule MediumGraphqlApi.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
   schema "users" do
-    field :role, :string
+    field :role, :string, default: "user"
     field :first_name, :string
     field :last_name, :string
     field :email, :string
     field :hashed_password, :string
+    field :password, :string, virtual: true
+    field :password_confirmation, :string, virtual: true
 
     timestamps(type: :utc_datetime)
   end
@@ -15,7 +19,24 @@ defmodule MediumGraphqlApi.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:first_name, :last_name, :email, :hashed_password, :role])
-    |> validate_required([:first_name, :last_name, :email, :hashed_password, :role])
+    |> cast(attrs, [:first_name, :last_name, :email, :password, :password_confirmation, :role])
+    |> validate_required([
+      :first_name,
+      :last_name,
+      :email,
+      :password,
+      :password_confirmation,
+      :role
+    ])
+    |> validate_format(:email, ~r/^[^\s]+@[^\s]+$/, message: "must have the @ sign and no spaces")
+    |> update_change(:email, &String.downcase/1)
+    |> validate_length(:password, min: 6, max: 15)
+    |> validate_confirmation(:password)
+    |> unique_constraint(:email)
+    |> hash_password()
+  end
+
+  defp hash_password(changeset) do
+    changeset
   end
 end
