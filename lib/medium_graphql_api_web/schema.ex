18d13e9d9dec2f91ebcedb6d2da1e9f9868df0cb
Rowa <jrowah@gmail.com>
2024-10-15 14:27:54 +0300

    setup schema file

---
+++ lib/medium_graphql_api_web/schema.ex
@@ -0,0 +1,14 @@
+defmodule MediumGraphqlApiWeb.Schema do
+  use Absinthe.Schema
+
+  # import_types MediumGraphqlApiWeb.Schema.Types
+
+  query do
+  end
+
+  # mutation do
+  # end
+
+  # subscription do
+  # end
+end
